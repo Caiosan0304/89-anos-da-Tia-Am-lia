0ad6bb73c4f42c9e9bf6e40d499997f1f15b89ca
Caiosan0304 <98545805+Caiosan0304@users.noreply.github.com>
2025-08-19 01:50:41 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'tia-amelia-v1';
+    const ASSETS = ['/', '/index.html'];
+    self.addEventListener('install', e => {
+      e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+    });
+    self.addEventListener('activate', e => { e.waitUntil(self.clients.claim()); });
+    self.addEventListener('fetch', e => {
+      const req = e.request;
+      e.respondWith(
+        caches.match(req).then(cached => cached || fetch(req).then(res=>{
+          const copy = res.clone();
+          caches.open(CACHE).then(c=> c.put(req, copy));
+          return res;
+        }).catch(()=> cached))
+      );
+    });
